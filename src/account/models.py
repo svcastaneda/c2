360de4b4644d1ceeaeae6dfd7818596aad1c9e70
Dan Conrad <danmconrad@users.noreply.github.com>
2018-03-17 14:28:51 -0500

    Adds autoformatting (#9)

--- src/account/models.py
+++ src/account/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import UserManager as DefaultUserManager, AbstractUser
 
+
 class UserManager(DefaultUserManager):
     def create_user(self, email=None, password=None, **extra_fields):
         super().create_user(username=uuid.uuid4(), email=email, password=password, **extra_fields)
@@ -14,7 +15,7 @@ class User(AbstractUser):
     objects = UserManager()
 
     email = models.EmailField(unique=True,
-        error_messages={'unique': 'A user with that email already exists.'})
+                              error_messages={'unique': 'A user with that email already exists.'})
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- src/tasks.py
+++ src/tasks.py
@@ -1,5 +1,6 @@
 from invoke import task
 
+
 @task
 def start(ctx):
     build(ctx)
@@ -9,17 +10,21 @@ def start(ctx):
     # Must be last- this is a long running process
     serve(ctx)
 
+
 @task
 def build(ctx):
     ctx.run("./manage.py collectstatic --no-input")
+
 
 @task
 def migrate(ctx):
     ctx.run("./manage.py migrate")
 
+
 @task
 def serve(ctx):
     ctx.run("gunicorn dojo.wsgi -b 0.0.0.0:8000")
+
 
 @task
 def load_dev_fixtures(ctx):
@@ -27,6 +32,13 @@ def load_dev_fixtures(ctx):
     # Check to make sure this isn't being run in production
     ctx.run("./manage.py loaddata account/fixtures/*")
 
+
+@task
+def format(ctx):
+    ctx.run("autopep8 -iaarj4 --exclude=\"*/migrations/*\" --max-line-length=\"120\" .")
+
+
 @task
 def test(ctx):
-    pass
+    format(ctx)
+    no_uncommitted_changed(ctx)
