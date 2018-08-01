0aba16b803a5425db746847922fa751512a4c9b6
danmconrad <dan.m.conrad@gmail.com>
2018-08-01 15:03:33 -0500

    Use sendgrid instead of mailgun

--- config/settings/production.py
+++ config/settings/production.py
@@ -132,11 +132,11 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
 INSTALLED_APPS += ['anymail']  # noqa F405
-EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+EMAIL_BACKEND = 'anymail.backends.sendgrid.EmailBackend'
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 ANYMAIL = {
-    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
+    'SENDGRID_API_KEY': env('SENDGRID_API_KEY'),
+    'SENDGRID_SENDER_DOMAIN': env('SENDGRID_DOMAIN')
 }
 
 # Gunicorn
